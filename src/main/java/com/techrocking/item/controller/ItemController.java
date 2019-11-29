94ca2d0f96969b18c657acbcbc760e4669de3061
rajesh <rajwaitu@gmail.com>
2019-11-29 16:23:42 +0530

    updating context url

--- src/main/java/com/techrocking/item/controller/ItemController.java
+++ src/main/java/com/techrocking/item/controller/ItemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techrocking.item.data.converter.ItemDataConverter;
@@ -19,7 +18,6 @@ import com.techrocking.item.payload.SaveItemResponse;
 import com.techrocking.item.service.ItemService;
 
 @RestController
-@RequestMapping("/item")
 public class ItemController {
 	
 	@Autowired
